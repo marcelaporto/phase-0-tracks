c77b951c26da1a1aa14b2f2bcf91f292a6d1854c
Marcela Porto <marcela.fporto@gmail.com>
2017-01-06 19:01:11 -0200

    Gather data about suspicious werewolf's preferences

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,3 +10,9 @@ age = gets.chomp
 puts "What year were you born?"
 birth_y = gets.chomp
 
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health = gets.chomp
+
