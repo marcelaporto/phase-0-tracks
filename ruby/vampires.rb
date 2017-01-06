006697afda8c4fdf7d9d56fb88fd8008ead9f06b
Marcela Porto <marcela.fporto@gmail.com>
2017-01-06 19:00:03 -0200

    Gather data from the suspicious werewolf

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+
+puts "Welcome to our interview!"
+puts "What is your name"
+name = gets.chomp
+puts "Nice to meet you #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_y = gets.chomp
+
