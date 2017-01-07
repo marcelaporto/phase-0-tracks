e2d74c125fddefa0963b0637bab1385e73e5d51c
Marcela Porto <marcela.fporto@gmail.com>
2017-01-07 19:49:13 -0200

    Add pseudocode to Encrypt ande Decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,21 @@
+=begin
+pseudocode
+1) encrypt
+- Loop through each letter (word lenght)
+- When you get to the letter,
+- If it is equal to space, leave it that way
+	- Else, this letter becomes the next (use bang operator)
+- Counter goes to next letter
+- In the end, print 
+
+2)decrypt
+- Define "abcdefghijklmnopqrstuvwxyz" as string
+- Loop through each letter (word lenght)
+- When you get to the letter,
+- If it is equal to space, leave it that way
+	- Else, ask index of the letter, and equal as new variable
+		- letter becomes new variable -1  of the alphabet string
+- Counter goes to next letter
+- In the end, print 
+
+=end
