4ef1ffbc015bb2f88b40e1eb8fe32bebf159ca35
Marcela Porto <marcela.fporto@gmail.com>
2017-01-07 03:21:27 -0200

    Turned garlic and health variables into booleans & generated variable to check if age and birth year match

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -27,3 +27,27 @@ puts "Age: #{age}"
 puts "Birth Year: #{birth_y}"
 puts "Garlic Lover? #{garlic}"
 puts "Health Insurence: #{health}"
+
+#Generate age to check with the age given
+birth_y_result = 2016 - birth_y
+
+#Turn strings into booleans
+if garlic=="y"
+	garlic=true
+else
+		garlic=false
+end 	
+
+
+if health=="y"
+	health=true
+else
+		health=false
+end 	
+
+
+#Analisys of vampire condition
+if birth_y_result == age && (garlic || health)
+	result = "Probably not a vampire"
+elsif birth_y_result != age &&
+		
