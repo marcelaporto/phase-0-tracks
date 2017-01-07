653f44615021f2e285499a7872e1f501d25d8eed
Marcela Porto <marcela.fporto@gmail.com>
2017-01-07 02:54:00 -0200

    Add summary of data collected

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,9 @@
 
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
 puts "Welcome to our interview!"
 puts "What is your name"
 name = gets.chomp
@@ -13,6 +18,12 @@ birth_y = gets.chomp
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
 health = gets.chomp
 
+#Summary of the data collected
+puts "So what we now about #{name}"
+puts "Age: #{age}"
+puts "Birth Year: #{birth_y}"
+puts "Garlic Lover? #{garlic}"
+puts "Health Insurence: #{health}"
