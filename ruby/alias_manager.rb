532daa052eb3628904213692d2fe71b413d7d338
Marcela Porto <marcela.fporto@gmail.com>
2017-01-13 17:26:13 -0200

    Add method to swap name, swap vowel, swap consonant, and put it all together

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,7 +15,63 @@ PSEUDOCODE
 
 =end
 
+def swap_names(name)
+	split_name = name.split(" ")
+	split_name[0], split_name[1] = split_name[1], split_name[0]
+	split_name.join(" ")
+end
 
+def swap_vowel(char)
+	vowels = ["a", "e", "i", "o", "u"]
+	i = 0
+	vowels.each do |vowel|
+		if char == vowel
+			char = vowels[i + 1]
+			return char
+		else
+		  i += 1
+		end
+	end
+
+end
+
+def swap_consonant(char)
+	consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'] #got it from the internet
+	i = 0
+	consonants.each do |consonant|
+		if char == consonant
+			char = consonants[i + 1]
+			return char
+		else
+		  i += 1
+		end
+	end
+
+end
+
+end
+
+
+def new_name(name)
+#method that makes swap words
+name = swap_names(name)
+
+#split into chars
+name_array = name.chars
+
+name_array.each do |char|
+	if char = " "
+		char = " "
+	elsif char = "a" || "e" || "i" || "o" || "u"
+		char = swap_vowel(char)
+	else
+		char = swap_consonant(char)
+	end
+end
+
+new_name = name_array.join("")
+#do I have to return?
+end
 
 
 
@@ -24,6 +80,7 @@ PSEUDOCODE
 puts "Hello! What is your name?"
 name = gets.chomp
 
-new_name = fake_name(name)
+fake_name = new_name(name)
 
-puts "Your fake name is #{new_name}"
+puts "Your fake name is #{fake_name}"
+puts fake_name.class
