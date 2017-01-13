530dfd51502856f6552d8fef7dee6c209bd75093
Marcela Porto <marcela.fporto@gmail.com>
2017-01-13 02:47:06 -0200

    Add prints of nested elements

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -24,7 +24,7 @@ university = {
 	    focused:0.25, 
 	    sports_driven: 0.20
 	  },
-	  students_housing: ["dorms", "frat houses", "fraternity houses", "shared apartment"],
+	  students_housing: ["Dorms", "Frat Houses", "Sorority houses", "Shared apartment"],
 	},
 	major_sport: "Fantasy Futball",
 	known_for: "Best history major in the US",
@@ -34,3 +34,47 @@ university = {
 
 
 puts university
+puts nil
+
+
+#Print out nested items
+puts university [:major_sport]
+p university[:teachers_type]
+puts university[:students][:types_students] 
+puts university[:students][:students_housing][0] 
+puts university[:department_social_studies][:philosophy] 
+
+# Add one item
+
+puts nil
+puts university
+puts nil
+
+university[:teachers_type].push("Old non-tech lady/sir") #Add to array
+university[:students][:types_students][:glee_driven] = 0.002 #Add to hash
+university[:department_math][:metaphysics] = "superhard"
+
+
+# Overwrite one item
+
+puts university
+puts nil
+
+university[:major_sport] = "Wii Sports"
+university[:students][:students_housing][3] = "Hippie Cabins"
+
+
+# Use three other built in methods 
+
+puts university
+puts nil
+
+university[:department_math][:metaphysics].upcase!
+university[:students][:students_housing].reverse!
+
+puts a = university[:students][:types_students].length
+puts nil
+
+puts university
+puts nil
+
