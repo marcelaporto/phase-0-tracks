b4ce3a2cf34bb0b30735eaf91c5376a53aefcd2b
Marcela Porto <marcela.fporto@gmail.com>
2017-01-13 18:41:53 -0200

    Made code more readable

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,31 +15,36 @@ PSEUDOCODE
 
 =end
 
+#Methods to create a fake name
+
 def swap_names(name)
 	split_name = name.split(" ")
 	split_name[0], split_name[1] = split_name[1], split_name[0]
 	split_name.join(" ")
 end
+
 def swap_vowel(char)
 	vowels = ["a", "e", "i", "o", "u"]
 	i = 0
+	
 	if char == "u"
-		   char = "a"
+		char = "a"
 	else vowels.each do |vowel|
-		  if char == vowel
-			    char = vowels[i + 1]
+		if char == vowel
+		    char = vowels[i + 1]
 			return char
 		else
-		  i += 1
+		 	i += 1
 		end
 	end
 	end
-
 end
+
 
 def swap_consonant(char)
 	consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] #got it from the internet
 	i = 0
+	
 	if char == "z"
 	   char = "b"
 	else consonants.each do |consonant|
@@ -55,14 +60,21 @@ end
 
 
 def name_capitalize(name_array)
+
 new_name = name_array.join("")
 new_name = new_name.split
+
 new_name[0].capitalize!
 new_name[1].capitalize!
+
 new_name = new_name.join(" ") 
+
 end
 
+#Method that uses all fo the methods above
+
 def new_name(name)
+
 #method that swap words
 name = swap_names(name)
 
@@ -70,7 +82,7 @@ name = swap_names(name)
 name = name.downcase!
 name_array = name.chars
 
-# Conditional to aplly different methods
+# Conditional to aplly different methods, depending on the type of char
 name_array.map! do |char|
 	if char == " "
 		char = " "
@@ -99,6 +111,7 @@ fake_names = []
 #Counter of the times the program has generated a fake name
 n = 0
 
+
 until name == "Exit"
 	real_names << name 
 	
@@ -112,6 +125,8 @@ until name == "Exit"
 	name = gets.chomp
 	n += 1
 end
+
+#Prints results when the user doesn't want to generate new names
 
 puts nil
 puts "Results:"
