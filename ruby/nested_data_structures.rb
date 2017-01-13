07bb141954ce3367ff3077d0058d02f56567330f
Marcela Porto <marcela.fporto@gmail.com>
2017-01-13 02:02:32 -0200

    Add nested structure - university

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+university{
+	department_health: {
+		nursery: "easy"
+		biology: "medium"
+		medicine: "hard"
+	}
+	department_social_studies:{
+		philosophy: "easy"
+		history: "medium"
+		economics: "hard"
+
+	}
+	department_math: {
+		engineering: "easy"
+		conceptual_math: "medium"
+		physics: "hard"
+	}
+	Student_housing: ["dorms", "frat houses", "fraternity houses", "shared apartment"]
+	Teachers: ["Academic", "Part time workers in the market", "Helps around with extracurricular activities"]
+	Students: ["Lazy", "Focused", "Sports-Driven"]
+	Major_Sport: "Fantasy Futball"
+	Known_for: "Best history major in the US"
+	Miles_urban_city: 220
+	Tech_school: false
+}
+
+print university
