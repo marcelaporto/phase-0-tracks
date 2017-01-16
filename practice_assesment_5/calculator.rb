ef06a9a2cd6d6300c6de4c9db0f0654f95cf239f
Marcela Porto <marcela.fporto@gmail.com>
2017-01-16 02:48:45 -0200

    Add new driver code, acording to question3

--- practice_assesment_5/calculator.rb
+++ practice_assesment_5/calculator.rb
@@ -5,7 +5,20 @@ def calculator(integer1,operator_s,integer2)
 	integer1.send(operator_s,integer2) #use of Object#send to convert string into operator
 end
 
+puts "Please enter the operation you want to do"
+operation = gets.chomp
 
+#Prepare variables to apply method
+array_of_variables = operation.chars
+integer1 = array_of_variables[0].to_i
+operator_s = array_of_variables[1]
+integer2 = array_of_variables[2].to_i
+
+puts calculator(integer1,operator_s,integer2)
+
+
+
+=begin
 #DRIVER CODE
 
 puts "Please define your first integer"
@@ -20,3 +33,4 @@ puts "Result: #{integer1} plus #{integer2} is #{calculator(integer1,"+",integer2
 puts "Result: #{integer1} minus #{integer2} is #{calculator(integer1,"-",integer2)}"
 puts "Result: #{integer1} divided by #{integer2} is #{calculator(integer1,"/",integer2)}"
 puts "Result: #{integer1} times #{integer2} is #{calculator(integer1,"*",integer2)}"
+=end
