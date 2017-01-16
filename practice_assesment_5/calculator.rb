6373a470bb25d788928b87532b743256e393b728
Marcela Porto <marcela.fporto@gmail.com>
2017-01-16 02:39:23 -0200

    Add driver code

--- practice_assesment_5/calculator.rb
+++ practice_assesment_5/calculator.rb
@@ -2,8 +2,21 @@
 
 
 def calculator(integer1,operator_s,integer2)
-	result = integer1.send(operator_s,integer2) #use of Object#send to convert string into operator
-	return result
+	integer1.send(operator_s,integer2) #use of Object#send to convert string into operator
 end
 
-calculator(4,"+", 5)
+
+#DRIVER CODE
+
+puts "Please define your first integer"
+integer1 = gets.chomp
+integer1 = integer1.to_i
+
+puts "Please define your second integer"
+integer2 = gets.chomp
+integer2 = integer2.to_i
+
+puts "Result: #{integer1} plus #{integer2} is #{calculator(integer1,"+",integer2)}"
+puts "Result: #{integer1} minus #{integer2} is #{calculator(integer1,"-",integer2)}"
+puts "Result: #{integer1} divided by #{integer2} is #{calculator(integer1,"/",integer2)}"
+puts "Result: #{integer1} times #{integer2} is #{calculator(integer1,"*",integer2)}"
