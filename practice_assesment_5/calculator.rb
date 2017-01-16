b6b3441165bfc767d29d7eb0b539d76ad0f4f3bd
Marcela Porto <marcela.fporto@gmail.com>
2017-01-16 03:11:07 -0200

    Store calculations made to print them out in the end

--- practice_assesment_5/calculator.rb
+++ practice_assesment_5/calculator.rb
@@ -5,17 +5,36 @@ def calculator(integer1,operator_s,integer2)
 	integer1.send(operator_s,integer2) #use of Object#send to convert string into operator
 end
 
+i = 0 #counter
+hash_calculator = Hash.new #new hash
+
 puts "Please enter the operation you want to do"
 operation = gets.chomp
-operation = operation.delete(" ") #remove all whitespace between operation, if the user writes it like that
+
+
+while operation != "Done"
 
 #Prepare variables to apply method
-array_of_variables = operation.chars
+array_of_variables = operation.split
 integer1 = array_of_variables[0].to_i
 operator_s = array_of_variables[1]
 integer2 = array_of_variables[2].to_i
 
-puts calculator(integer1,operator_s,integer2)
+result = calculator(integer1,operator_s,integer2)
+hash_calculator["#{operation}"] = result #Insert operation and result in hash 
+
+puts "The result is #{result}"
+
+puts "Any other operations? Type it in or write down 'Done'"
+operation = gets.chomp
+
+i += 1
+
+end
+
+#Print out results
+puts "#{i} calculations performed"
+hash_calculator.each {|calculation, result| puts " #{calculation} = #{result}"}
 
 
 
