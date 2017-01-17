9028f47c65d7a693a96309206f3274ba931e0306
Marcela Porto <marcela.fporto@gmail.com>
2017-01-17 16:35:25 -0200

    Add Class Design to puppies

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+=begin
+Class Design: Puppies
+
+SPECIES ------------------------
+Puppies
+
+ATTRIBUTES ----------------
+Tongue lenght: varies
+Hours sleep/day: 8+
+Friendly: yes
+Name: varies
+Fur Color: varies
+
+METHODS -----------------------
+Jump
+Lick
+Bark
+Roll-over
+Play dead
+Sniff
+		
+=end
