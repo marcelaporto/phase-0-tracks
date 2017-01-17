57566659f0e20514b2ecee92ef6d5ee9535bd623
Marcela Porto <marcela.fporto@gmail.com>
2017-01-17 19:11:28 -0200

    Did release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,39 @@
 class Puppy
 
   def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+	puts "I brought back the #{toy}!"
+	toy
+  end
+
+
+  def speak(number)
+  	number.times do
+  		puts "Woolf!"
+  	end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years (age)
+  	dog_age = age*7
+  	puts dog_age	
+  end
+
+  def high_five(dog_name)
+  	dog_name.capitalize!
+  	puts "#{dog_name}, high-five!"
+  	puts "*high fives*"
   end
 
 end
 
+#DRIVER CODE
+
+roxy = Puppy.new
+roxy.fetch("plastic_bone")
+roxy.speak(4)
+roxy.roll_over
+roxy.dog_years(5)
+roxy.high_five("roxy")
