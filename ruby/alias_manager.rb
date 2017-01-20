bb7a43ecc78847e9626b0c85e89b65905113c5b3
Marcela Porto <marcela.fporto@gmail.com>
2017-01-20 18:15:50 -0200

    Merge swap consonant and swap vowels methods to make code more simple

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,21 +13,6 @@ PSEUDOCODE
 	- Join it back to a string (define as a new variable and make sure it is a string with .class)
 - Give it back to the user
 
-
-CHANGES
-
-1. 
-In your swap_vowel and swap_consonant methods, can you think of a more efficient way to find the next letter 
-that does not involve iterating over the entire collection of vowels or consonants in order to find the letter 
-that you passed into the method? Hint...there is a method called .index() that you can use to find the position 
-of an element in an array or string. 
-
-2. 
-The bodies of your swap_vowel and swap_consonant methods are almost exactly the same with the exception of which 
-collection of letters you are using and the edge cases. Can you think of a way to make a more general method that 
-could handle either consonants or vowels, preventing you from having to repeat yourself at all? Hint...you can pass 
-the appropriate collection of letters into the method so that the appropriate array of letters is available to you.
-
 =end
 
 #Methods to create a fake name
@@ -38,41 +23,18 @@ def swap_names(name)
 	split_name.join(" ")
 end
 
-def swap_vowel(char)
-	vowels = ["a", "e", "i", "o", "u"]
-	i = 0
+def swap_letter(char)
+	letters = ['a', 'e', 'i', 'o', 'u','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] 
 	
-	if char == "u"
-		char = "a"
-	else vowels.each do |vowel|
-		if char == vowel
-			char = vowels[i + 1]
-			return char
-		else
-			i += 1
-		end
+	if char == 'z'
+		char = 'b'
+	elsif char == 'u'
+		char = 'a' 
+	else 		
+		i = letters.index(char)
+		char = letters[i + 1]
 	end
 end
-end
-
-
-def swap_consonant(char)
-	consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] #got it from the internet
-	i = 0
-	
-	if char == "z"
-		char = "b"
-	else consonants.each do |consonant|
-		if char == consonant
-			char = consonants[i + 1]
-			return char
-		else
-			i += 1
-		end
-	end
-end
-end
-
 
 def name_capitalize(name_array)
 
@@ -86,30 +48,29 @@ def name_capitalize(name_array)
 
 end
 
-#Method that uses all fo the methods above
+#Method that uses all of the methods above
 
 def new_name(name)
 
-#method that swap words
-name = swap_names(name)
+	#1. Method that swap words
+	name = swap_names(name)
 
-#split into chars
-name = name.downcase!
-name_array = name.chars
+	#2. Split into chars
+	name = name.downcase!
+	name_array = name.chars
 
-# Conditional to aplly different methods, depending on the type of char
-name_array.map! do |char|
-	if char == " "
-		char = " "
-	elsif char == "a" || char == "e" ||  char == "i" || char == "o" || char ==  "u" #tried to put it all in between () and wasn't succesful, why is that?
-		char = swap_vowel(char)
-	else
-		char = swap_consonant(char)
+	#3. Conditional to aplly different methods, depending on the type of char
+	name_array.map! do |char|
+
+		if char == " "
+			char = " "
+		else
+			char = swap_letter(char)
+		end
 	end
-end
 
-#Capitalize first two letters of the names
-name_capitalize(name_array)
+	#4. Capitalize first two letters of the names
+	name_capitalize(name_array)
 
 end
 
@@ -124,7 +85,6 @@ name_transformation = Hash.new
 
 
 until name == "Exit"
-	
 	fake_name = new_name(name)
 
 	name_transformation[name] = fake_name
