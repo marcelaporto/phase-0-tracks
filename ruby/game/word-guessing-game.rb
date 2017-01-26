eb3000c73f60159a216eb9b1b106189880b04f4f
Marcela Porto <marcela.fporto@gmail.com>
2017-01-25 23:58:39 -0200

    Refactor to the limit

--- ruby/game/word-guessing-game.rb
+++ ruby/game/word-guessing-game.rb
@@ -6,154 +6,141 @@
 
 class Game
 
-  attr_reader :word_array, :guesses_made, :number_slots_array, :word, :array_repeated
+  attr_reader :word, :guesses_made
 
-  # FEEDBACK
-  # Currently, the only method you are calling from outside your class is the check_guess method. 
-  # Consider moving this method up to the top of your class (but still below your initialize method) 
-  # and adding a private declaration to protect your other methods from being accessed from outside the # class
-
-
-  # Creates instance variables - word (that will be guessed), number_slots_array (tool to check guess),
-  # guesses_made (hash that defines which guesses were made, and if they were wrong or not), word_array(word divided into an array).
+  # Creates instance variables - word (that will be guessed), puzzle_view (tool to check guess),
+  # guesses_made (hash that defines which guesses were made, and if they were wrong or not), word_chars(word divided into an array).
   # Output: none 
-  def initialize(word)
+  def initialize
     @word = word
-    @number_slots_array = Array.new(word.length).fill("_")
+    @puzzle_view = []
     @guesses_made = Hash.new
-    @word_array = @word.chars
-    @array_repeated = Array.new
+    @array_repeated = []
+    @guess = ""
+    @win = false
+  end
+
+  def play
+    game_intro
+    take_turn
+    final_message
+  end
+
+private
+
+  def game_intro
+    print "Welcome to the game player 1, insert your word\n"
+
+    @word = gets.chomp
+    @word.downcase!
+    @puzzle_view = Array.new(@word.length).fill("_")
+    @number_guesses = @word.length + 2 # (number of guesses available is related to the length of the word)
+
+    print " \nOk, please hand the game to player 2.\n Welcome player 2. \n"
+  end
+
+  def take_turn
+    until game_over?
+    display_progress
+    get_guess
+    check_guess(@guess) if valid?
+    update_word(@guess)
+    display_progress
+    end
   end
 
 
-  # FEEDBACK
-  # is_repeated_in_word method, you can give a block to the count method in order to count 
-  # the number of items in an array that satisfy the condition in the block. This would prevent 
-  # you from having to map over the array to produce true/false values, having to iterate over those 
-  # values to remove the false ones, and then having to count the length of the resulting array, replacing 
-  # all of those with word_array.count{ |letter| letter == guess }   ...which would return the number of times 
-  # the guessed letter appeared in the word_array.
-
+  def final_message
+    print "\n#{@puzzle_view.join(" ")}\n"
     
-  # Creates an array in which the chars that have the player's guess becomes "true", otherwise they become
-  # false. After that, deleted all false chars, and counted the length of the array. If its length >1, it means
-  # this letter, the player's guess, appears more than once in the word (thus, is_repeated_in_word? = true).
-  # Output: boolean
-  def is_repeated_in_word?(guess) 
-
-    # To find out if guess is a letter that is in the word more than once:
-    @array_repeated = @word_array.each_index.select { |i| @word_array[i] == guess }
-    # Output: an array of the index numbers of when this letter is in the word
-    
-    # If the length of the array is >1, that means that the word has the letter that the player guessed more than once           
-    if @array_repeated.length == 1
-      false
+    if win?
+      print "\nYOU ARE THE BEST, THE WORD WAS #{word.upcase}\n"
     else 
-      true
+      print "\nBlÉ YOU LOST, THE WORD WAS #{word.upcase}\n"
     end
-
+  print "\nThanks for playing!"
   end
 
-  # Updates the word according to the guesses_made hash, only inserting the ones that value as true.
-  # Output: number_slots_array - String
-  def update_word(guesses_made)
+  def win?
+    !@puzzle_view.include?('_')
+  end
 
-    @guesses_made.each do |guess, nature|
+  def game_over?
+      @guesses_made.length >= @number_guesses  || !@puzzle_view.include?('_')
+  end
 
-      if nature && is_repeated_in_word?(guess)
-        @array_repeated.each { |index| number_slots_array[index] = guess} # will go through the array_repeated, and insert the guess in each part of number_slots_array that match
-      elsif nature && !is_repeated_in_word?(guess)
-        i = word.index(guess)
-        number_slots_array[i] = guess
-      end
-
-    end
-    
-    number_slots_array.join (" ")
-
+  def display_progress
+    @puzzle_view.join (" ")
   end
 
   # Checks if letter guessed is inside the word (if yes, = true; if no, = false). 
   # Add guess to the hash as the key, and the nature of it (true/false) as the value
-  # Output(uses update_word to Output): number_slots_array.join(" ") - String that dislays to the user
+  # Output(uses update_word to Output): puzzle_view.join(" ") - String that dislays to the user
   def check_guess(guess)
     indx = word.index(guess)
 
     if indx == nil
-      puts nil # extra space
-      puts "This letter is not in it!"
-      puts nil # extra space
-      guesses_made[guess] = false
+      #puts nil # extra space
+      print "\nThis letter is not in it!\n"
+      @guesses_made[@guess] = false
     else 
-      puts nil # extra space
-      puts "You're right!"
-      puts nil # extra space
-      guesses_made[guess] = true
+      print "\nYou're right!\n"
+      @guesses_made[@guess] = true
     end
-        
-    guesses_made
-    update_word(guesses_made)
-
   end
 
-  # Takes the length of the word and adds two, to define how many tries does the player have
-  # (number of guesses available is related to the length of the word)
-  # Output: number_guesses -> <integer>
-  def number_guesses
-    @word.length + 2
+  def get_guess
+    print @puzzle_view.join(" ")
+    print "\n\nYou have exactly #{@number_guesses - guesses_made.length} guess(es) left" 
+    print "\nPlease, make a guess (only one letter per guess)\n"
+    @guess = gets.chomp
   end
 
+  def valid?
+    if @guesses_made.key?(@guess) 
+    print "\nYou have already guessed that!\n"
+    get_guess
+    else 
+    true
+    end
+  end
+
+
+  # Updates the word according to the guesses_made hash, only inserting the ones that value as true.
+  # Output: puzzle_view - String
+  def update_word(guess)
+
+    @word.split("").each.with_index do |letter, index|
+      if letter == @guess 
+        @puzzle_view[index] = guess
+      end
+    end
+  end
 end
 
 
 # DRIVER CODE
 
-# Player 1
+game = Game.new
+game.play
 
-puts "Welcome to the game player 1, insert your word"
 
-player1_word = gets.chomp
-player1_word.downcase!
+# ALTERNATIVE SOLUTION TO INSERT REPEATED LETTERS
+#
+#   # Creates an array in which the chars are the position that the guess appears in the word. If its length >1, it means
+#   # this letter, the player's guess, appears more than once in the word (thus, is_repeated_in_word? = true).
+#   # Output: boolean (& array of position to use afterwards)
+#   def is_repeated_in_word?(guess) 
 
-game = Game.new(player1_word)
+#     # To find out if guess is a letter that is in the word more than once:
+#     @position_letter = @word_chars.each_index.select { |i| @word_chars[i] == guess }
+#     # Output: an array of the index numbers of when this letter is in the word
+    
+#     # If the length of the array is >1, that means that the word has the letter that the player guessed more than once           
+#     if @position_letter.length == 1
+#       false
+#     else 
+#       true
+#     end
 
-puts "Ok, please hand the game to player 2."
-
-# Player 2
-
-puts "Welcome player 2,the word you are going to guess looks like this: #{game.number_slots_array.join (" ")} "
-
-guesses_counter = 0
-
-while guesses_counter  < game.number_guesses 
-  puts "You have exactly #{game.number_guesses - guesses_counter} guess(es) left"
-  puts nil  
-  puts "Please, make a guess (only one letter per guess)"
-  guess = gets.chomp
-
-  # If guess was already made (thus, this key is already in the "guesses_made" hash), p2 doesn't lose a round and tries again
-  if game.guesses_made.key?(guess) == true 
-    puts "You have already guessed that!"
-  else 
-    puts game.check_guess(guess)
-    guesses_counter  += 1
-  end
-
-  puts nil
-
-  # Check if the number_slots_array still have "_" - if not, game is over; otherwise, go again
-  break if (game.number_slots_array.include? "_") == false
-
-end
-
-puts nil 
-
-# Final Remarks
-if (game.number_slots_array.include? "_") == true # Check again to make final remarks
-    puts "You lost! The word was #{game.word}"
-else
-    puts "You won! Congratulations"
-end
-
-puts "Thanks for playing!"
-
+#   end
