562500c88638b32aed19b78577db152b378e2633
Marcela Porto <marcela.fporto@gmail.com>
2017-02-07 16:14:21 -0200

    Do Release 2

--- web_dev/jquery/client_side_js/chameleon/client_javascript.js
+++ web_dev/jquery/client_side_js/chameleon/client_javascript.js
@@ -3,7 +3,6 @@ var colors = ['red', 'green', 'blue'];
 
 function changeColor(event){
 	event.target.style.border = "3px solid " + colors[0];
-
 	rotate(colors,1);
 }
 
@@ -14,5 +13,19 @@ function rotate( array , times ){
    }
 }
 
+function popUp (event){
+	gif.hidden = false;
+}
+
+
+var gif = document.getElementById('gif')
+gif.hidden = true;
+
+
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", changeColor);
+
+
+var buttons = document.getElementsByTagName('button');
+var button = buttons[1];
+button.addEventListener("click", popUp);
